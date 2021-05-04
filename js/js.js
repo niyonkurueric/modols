ff640f00c37dd41b21563e4ee631301a6e2eb96f
niyonkurueric <niyonkurufamous@gmail.com>
2021-05-04 17:39:17 +0200

    my works

---
+++ js/js.js
@@ -0,0 +1,13 @@
+
+const models=()=>
+{
+	document.getElementById('modelbody').style.display="block";
+}
+const button=()=>
+{
+	document.getElementById('modelbody').style.display="none";
+}
+const modelheadclase=()=>
+{
+	document.getElementById('modelbody').style.display="none";
+}
